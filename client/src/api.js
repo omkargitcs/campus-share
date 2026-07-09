623043a6c342bfc0bb9cdaf356a93424490163ab
omkargitcs <bhalsingom04@gmail.com>
2026-07-09 23:08:07 +0530

    updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,16 +1,25 @@
 import axios from "axios";
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL
     ? `${import.meta.env.VITE_API_URL}/api`
     : "http://localhost:5000/api",
 });
 
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
-});
+// ➔ Bulletproof Request Interceptor
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      // ✅ Modern and secure way to set request headers in Axios
+      config.headers = config.headers || {};
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default API;
